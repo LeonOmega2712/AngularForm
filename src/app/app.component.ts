4f9929c654813260d94bfa9f1869a8bc970b129c
Jonathan Muñoz <jonathan2712emr@gmail.com>
2020-10-23 12:30:11 -0500

    Update app.component.ts
    Se agregaron los imports de Angular/forms. se creó la estructura inicial del FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularForm';
+
+  formBuilder: FormGroup;
+
+  group: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    lastName: new FormControl("", [Validators.required]),
+    age: new FormControl("", [Validators.required]),
+    civilState: new FormControl("", [Validators.required]),
+    sex: new FormControl("", [Validators.required]),
+    comments: new FormArray([
+      new FormGroup({
+        comment: new FormControl("Add a comment here")
+      })
+    ]),
+  });
 }
